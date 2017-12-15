108dffbcc8890c19f3f82cbbdc2e6a0dad91082e
Remy Sharp <remy@remysharp.com>
2017-12-15 12:43:22 +0000

    test: more tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -6,19 +6,26 @@ const { spawn } = require('child_process');
 const pstree = require('../');
 const { tree, pidsForTree } = require('../lib/utils');
 
-test('constructs a tree', async t => {
-  const fixture = await readFile(__dirname + '/fixtures/out1', 'utf8');
+// test('constructs a tree', async t => {
+//   const fixture = await readFile(__dirname + '/fixtures/out1', 'utf8');
+//   const ps = await tree(fixture);
+//   t.deepEqual(pidsForTree(ps, 27).map(_ => _.PID), ['28', '39']);
+// });
+
+test('tree for live env', async t => {
+  const pid = 4079;
+  const fixture = await readFile(__dirname + '/fixtures/out2', 'utf8');
   const ps = await tree(fixture);
-  t.deepEqual(pidsForTree(ps, 27).map(_ => _.PID), ['28', '39']);
+  t.deepEqual(pidsForTree(ps, pid).map(_ => _.PID), ['4080']);
 });
 
 test('can read full child process tree', t => {
-  const subprocess = spawn(
+  const sub = spawn(
     'sh',
     [
       '-c',
       `node -e "setInterval(() => {
-        console.log(process.pid, 'is alive')
+        console.log(sub.pid, process.pid, 'is alive')
       }, 200);"`,
     ],
     {
@@ -27,12 +34,12 @@ test('can read full child process tree', t => {
   );
 
   setTimeout(() => {
-    const pid = subprocess.pid;
+    const pid = sub.pid;
     pstree(pid, (error, children) => {
-      t.pass('worked');
       children.concat({ PID: pid }).forEach(p => {
         spawn('kill', ['-s', 'SIGTERM', p.PID]);
       });
+      t.equal(children.length, 1);
       t.end();
     });
   }, 500);
